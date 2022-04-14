94a02b79bd318a346a803def88e03b7026ac22f2
Rayfranc13 <rayfranc13@gmail.com>
2022-04-14 19:16:25 -0400

    primer comit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const cors=require('cors')
 const mongoose=require('mongoose')
 const express =require("express") 
 const app= express()
@@ -9,6 +10,7 @@ mongoose.connect(process.env.MONGO_DB)
 
 
 app.use(express.json())
+app.use(cors())
 app.use('/producto',require('./src/routes/producto'))
 
 app.listen(process.env.PORT,(res=>console.log(`Server on port ${process.env.PORT}`)))
